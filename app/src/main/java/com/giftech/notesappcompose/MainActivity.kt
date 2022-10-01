c8a2f804cf7789a39ce1196bb4202211d05fcf01
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-10-01 15:41:20 +0700

    add notes state with add remove

--- app/src/main/java/com/giftech/notesappcompose/MainActivity.kt
+++ app/src/main/java/com/giftech/notesappcompose/MainActivity.kt
@@ -1,22 +1,28 @@
 package com.giftech.notesappcompose
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.giftech.notesappcompose.data.NoteDataSource
+import com.giftech.notesappcompose.model.Note
 import com.giftech.notesappcompose.screen.NoteScreen
 import com.giftech.notesappcompose.ui.theme.NotesAppComposeTheme
 
 @ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,10 +31,13 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
+                    val notes = remember{
+                        mutableStateListOf<Note>()
+                    }
                     NoteScreen(
-                        notes = NoteDataSource().loadNotes(),
-                        onAddNote = {},
-                        onRemoveNote = {}
+                        notes = notes,
+                        onAddNote = {notes.add(it)},
+                        onRemoveNote = {notes.remove(it)}
                     )
                 }
             }
