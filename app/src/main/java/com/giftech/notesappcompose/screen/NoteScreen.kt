32af5534df7721a8204f4eee51d3dc87008bc79f
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-10-01 15:14:44 +0700

    create note row

--- app/src/main/java/com/giftech/notesappcompose/screen/NoteScreen.kt
+++ app/src/main/java/com/giftech/notesappcompose/screen/NoteScreen.kt
@@ -1,25 +1,33 @@
 package com.giftech.notesappcompose.screen
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.giftech.notesappcompose.R
 import com.giftech.notesappcompose.components.NoteButton
 import com.giftech.notesappcompose.components.NoteInputText
+import com.giftech.notesappcompose.data.NoteDataSource
 import com.giftech.notesappcompose.model.Note
+import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
 @ExperimentalComposeUiApi
 @Composable
 fun NoteScreen(
@@ -63,7 +71,7 @@ fun NoteScreen(
             NoteButton(
                 text = "Save",
                 onClick = {
-                    if (title.isNotEmpty() && description.isNotEmpty()){
+                    if (title.isNotEmpty() && description.isNotEmpty()) {
                         //save to list
                         title = ""
                         description = ""
@@ -71,12 +79,58 @@ fun NoteScreen(
                 }
             )
         }
+        Divider(Modifier.padding(8.dp))
+        LazyColumn {
+            items(notes) { note ->
+                NoteRow(note = note, onNoteClick = {})
+            }
+        }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun NoteRow(
+    modifier: Modifier = Modifier,
+    note: Note,
+    onNoteClick: (Note) -> Unit
+) {
+    Surface(
+        modifier
+            .padding(4.dp)
+            .clip(RoundedCornerShape(topEnd = 16.dp, bottomStart = 16.dp))
+            .fillMaxWidth(),
+        color = MaterialTheme.colors.secondaryVariant,
+        elevation = 6.dp
+    ) {
+        Column(
+            modifier
+                .clickable { }
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                note.title,
+                color = MaterialTheme.colors.onSecondary,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Text(
+                note.description,
+                style = MaterialTheme.typography.subtitle1
+            )
+            Text(
+                note.entryDate.format(DateTimeFormatter.ofPattern("EEE, d MMM yy h a")),
+                        style = MaterialTheme.typography.caption
+            )
+
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
 @ExperimentalComposeUiApi
 @Preview(showBackground = true)
 @Composable
 fun NoteScreenPreview() {
-    NoteScreen(notes = emptyList(), onAddNote = {}, onRemoveNote = {})
+    NoteScreen(notes = NoteDataSource().loadNotes(), onAddNote = {}, onRemoveNote = {})
 }
