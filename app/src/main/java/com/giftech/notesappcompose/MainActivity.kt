cbbd70df8b323d39cf2caf88026aca8d0f36b40c
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-10-01 13:06:36 +0700

    show note screen on MainActivity.kt

--- app/src/main/java/com/giftech/notesappcompose/MainActivity.kt
+++ app/src/main/java/com/giftech/notesappcompose/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.giftech.notesappcompose.screen.NoteScreen
 import com.giftech.notesappcompose.ui.theme.NotesAppComposeTheme
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-
+                    NoteScreen()
                 }
             }
         }
